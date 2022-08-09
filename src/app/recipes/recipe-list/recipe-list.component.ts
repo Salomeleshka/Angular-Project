f7d8d7e9a6dba21ffa0076ed944f4c5e33944044
Salomeleshka <salomeleshka@gmail.com>
2022-08-09 14:37:05 +0200

    create models

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      '1 test Recipe',
+      'This is simply a test',
+      'https://media.self.com/photos/61e9dae8808d098c8ccc3b58/1:1/w_768,c_limit/15-Bean-Soup-Vegetarian.jpg'
+    ),
+    new Recipe(
+      '2 test Recipe',
+      'This is simply a test',
+      'https://theschmidtywife.com/wp-content/uploads/2021/02/Featured_Homemade_Pizza-720x720.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
