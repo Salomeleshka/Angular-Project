13f749d36889ac23d13238d216f16bf50711b051
Salomeleshka <salomeleshka@gmail.com>
2022-08-11 18:40:44 +0200

    add observable (subject)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  loadedfeature = 'recipe';
-  onNavigate(feature: string) {
-    this.loadedfeature = feature;
-  }
-}
+export class AppComponent {}
